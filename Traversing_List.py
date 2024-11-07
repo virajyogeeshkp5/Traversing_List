b9d2d5fdfb9e2371e5fea2f56e54efe1e8bed513
YOGEESH K P <yogeesh793@gmail.com>
2024-11-07 14:26:28 +0530

    Add files via upload

---
+++ Traversing_List.py
@@ -0,0 +1,23 @@
+num=[10,20,30,40,50,60]
+for i in num:
+    print(i+5)
+
+# while condition
+ind=0
+num=[10,20,30,40,50,60]
+while ind<len(num):
+    print(num[ind]+5)
+    ind=1+ind
+
+Li=[2,4,6,8,10]
+for i in Li:
+    print(i+5)
+
+for i in range(len(Li)):
+    print(num[i]+5)
+
+# while
+ind=0
+while ind<len(Li):
+    print(Li[ind]+5)
+    ind=1+ind    
